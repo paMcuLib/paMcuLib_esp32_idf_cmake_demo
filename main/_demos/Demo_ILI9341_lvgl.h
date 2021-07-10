e1775c535cfa0ebd5dfe437b709eb59350d3bb65
ActivePeter <1020401660@qq.com>
2021-07-11 04:58:08 +0800

    lvgl

--- main/_demos/Demo_ILI9341_lvgl.h
+++ main/_demos/Demo_ILI9341_lvgl.h
@@ -28,13 +28,13 @@ void timerCall_100us()
 #define MY_DISP_HOR_RES 240
 #define MY_DISP_VER_RES 320
 
-void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+void palv_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
-    // int32_t bufSize = (area->x2 - area->x1) * (area->y2 - area->y1);
+    int32_t bufSize = (area->x2 - area->x1) * (area->y2 - area->y1);
     /*It's a very slow but simple implementation.
      *`set_pixel` needs to be written by you to a set pixel on the screen*/
-    // appVar.lcd.setAddress(area->x1, area->y1, area->x2, area->y2);
-    // appVar.lcd.transmitBuf((uint8_t *)color_p, bufSize * 2);
+    appVar.lcd.setAddress(area->x1, area->y1, area->x2, area->y2);
+    appVar.lcd.transmitBuf((uint8_t *)color_p, bufSize * 2);
     _G_paBase.output("hello my_disp_flush\r\n");
     // for (y = area->y1; y <= area->y2; y++)
     // {
@@ -47,20 +47,21 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
 
     lv_disp_flush_ready(disp); /* Indicate you are ready with the flushing*/
 }
+lv_disp_drv_t disp_drv;
 void lv_app_init()
 {
     static lv_disp_draw_buf_t draw_buf;
-    static lv_color_t buf1[1]; /*Declare a buffer for 1/10 screen size*/
+    static lv_color_t buf1[MY_DISP_HOR_RES * MY_DISP_VER_RES / 10]; /*Declare a buffer for 1/10 screen size*/
     // sizeof(buf1);
     lv_disp_draw_buf_init(&draw_buf, buf1, NULL, MY_DISP_HOR_RES * MY_DISP_VER_RES / 10); /*Initialize the display buffer.*/
 
-    lv_disp_drv_t disp_drv;             /*Descriptor of a display driver*/
-    lv_disp_drv_init(&disp_drv);        /*Basic initialization*/
-    disp_drv.flush_cb = my_disp_flush;  /*Set your driver function*/
-    disp_drv.draw_buf = &draw_buf;      /*Assign the buffer to the display*/
-    disp_drv.hor_res = MY_DISP_HOR_RES; /*Set the horizontal resolution of the display*/
-    disp_drv.hor_res = MY_DISP_VER_RES; /*Set the verizontal resolution of the display*/
-    lv_disp_drv_register(&disp_drv);    /*Finally register the driver*/
+    // static      /*Descriptor of a display driver*/
+    lv_disp_drv_init(&disp_drv);         /*Basic initialization*/
+    disp_drv.flush_cb = palv_disp_flush; /*Set your driver function*/
+    disp_drv.draw_buf = &draw_buf;       /*Assign the buffer to the display*/
+    disp_drv.hor_res = MY_DISP_HOR_RES;  /*Set the horizontal resolution of the display*/
+    disp_drv.hor_res = MY_DISP_VER_RES;  /*Set the verizontal resolution of the display*/
+    lv_disp_drv_register(&disp_drv);     /*Finally register the driver*/
 }
 
 void mainAppEntry()
@@ -85,10 +86,10 @@ void mainAppEntry()
     while (1)
     {
         // appVar.lcd.flush(0, 0, 200, 200, 0x00ff);
-        _G_paBase.output("hello world\r\n");
+        // _G_paBase.output("hello world\r\n");
         _G_paBase.delayMs(1);
         lv_tick_inc(1);
-        lv_task_handler();
+        lv_timer_handler();
         // appVar.lcd.flush(0, 0, 200, 200, 0xff00);
         // _G_paBase.delayMs(2);
         // vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
